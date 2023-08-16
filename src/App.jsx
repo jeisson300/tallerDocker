64b378aaef3d658faf1f782e4991ed49960cad3c
jeisson <josejeisson00@hotmail.com>
2023-08-15 19:28:38 -0500

    punto 4

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import './App.css'
 function App() {
  
   const [form, setform] = useState({
-    name: ''
+    name: '',
+    cedula: ''
   })
+
+  const {name, cedula} = form;
 
   const handlechange = (e)=>
   {
-    setform({name: e.target.value})
+    
+    setform({ ...form , [e.target.name]: e.target.value})
   }
   const handleSendName = (e)=>
   {
@@ -24,26 +28,38 @@ function App() {
   })
   }
 
-  const handleToken = ()=>
+  const handleToken = (e)=>
   {
-    // const url = `http://localhost:32768/${}`;
+    e.preventDefault();
+    const url =`https://localhost:32768/generarToken`;
+    let variable = {
+      cedula, name
+    }
+    axios.post(url, variable).then((resp)=>
+    {
+      console.log(resp);
+    })
   }
 
-  const {name} =  form
+  
   return (
   <>
-    <div><h1>Punto 3 del taller</h1>
-    <form onSubmit={handleSendName}>
-      <p>
+    <div><h1>Punto 3 del taller</h1> 
+     <p>
 
       {/* <input type="text" placeholder='Ingrese un nombre' name = "name" value={name} onChange={handlechange} /> */}
       </p>
       <p>
-      <button type='submit'>ENVIAR NOMBRE AL SERVIDOR</button>
+      <button onClick={handleSendName}>ENVIAR NOMBRE AL SERVIDOR</button>
       
-      <button onclick='handleToken'>Token</button>
+      <form onSubmit={handleToken}>
+      <input type='text' placeholder='Nombre' name="name" value={name} onChange={handlechange}/>
+      <input type='text' placeholder='cedula'  name="cedula" value={cedula}  onChange={handlechange} />
+
+      <button type='submit'>Generar token</button>
+      </form> 
     </p>
-    </form>
+
     </div>
   </>
   )
